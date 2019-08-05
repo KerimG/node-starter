1ab0dcbbc3c60e2c8f4978fa99dd88992c66a0d9
Kerim Gueney <kerim.gueney@taskforce-it.de>
2019-08-05 13:07:08 +0200

    change ECMA version to avoid arrow function problem

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   extends: "eslint:recommended",
   parserOptions: {
-    ecmaVersion: 2015,
+    ecmaVersion: 2018,
   },
   rules: {
     "linebreak-style": ["error", "unix"],
